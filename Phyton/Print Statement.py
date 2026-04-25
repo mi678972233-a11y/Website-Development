eca34e18a72e2528fe3d242cac0c55d8819e925f
mi678972233-a11y <mi678972233@gmail.com>
2026-04-25 18:27:09 +0500

    Print Statement

---
+++ Phyton/Print Statement.py
@@ -0,0 +1,10 @@
+#Print A sentence
+print("Welcome To The World Of Programming !.")
+#Print A Number
+print(4)
+#Next Line After Print   
+print("Hello Python \n.")
+#Print All The Keywords In Python
+print("The Keywords In Python Are.....", )   
+import keyword
+print(keyword.kwlist)
