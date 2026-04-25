eef6dc850598cc096f3fd2c7de9e0bd3417a039b
mi678972233-a11y <mi678972233@gmail.com>
2026-04-25 18:19:20 +0500

    Print Statement And Comments

---
+++ Phyton/Print Statement And Comments.py
@@ -0,0 +1,8 @@
+#Printing A Greeting Message
+print("Hello, World!")
+# Printing My Name
+print("My Name Is M.Ibrahim")
+# Printing A Fun Fact About Myself
+print("I Love To Code In Python!")
+# Performing and Calculating A Simple Calculation
+print("The Result Of 5 And 3 Is:", 5 + 3)
