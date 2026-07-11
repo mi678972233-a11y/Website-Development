7468283f89151e73fffff155e36bd8f5c298a354
mi678972233-a11y <mi678972233@gmail.com>
2026-07-11 18:37:53 +0500

    Counting Game Points

---
+++ Phyton/Counting Game Points.py
@@ -0,0 +1,37 @@
+n = 4
+print("==== Counting Game Points (n=" , n , ", rounds=" , n , ") ====")
+print()
+total = n * (n + 1) // 2
+print("Formula way: Total =", total, "| steps = ", 1)
+total = 0
+steps = 0
+for round_num in range(1, n + 1):
+    total += round_num
+    steps += 1
+    print("Loop way: total =", total, "| steps = ", steps)
+total = 0
+steps = 0
+for round_num in range(1, n + 1):
+    for point in range(1, round_num + 1):
+        total += 1
+        steps += 1
+        print("Nested loop way: total =", total, "| steps = ", steps)
+n =10
+nested_steps = 0
+for round_num in range(1, n + 1):
+ for point in range(1, round_num + 1):
+    nested_steps += 1  
+print() 
+print("===now with n=", n, ", rounds===")
+print("Formula way: steps = ", 1 ,"(always just 1 !)")
+print("Loop way: steps = ", n)
+print("Nested loop way: steps = ", nested_steps)
+print("Same answer but very different coasts. This is time complexity!")
+
+
+
+      
+                                  
+
+
+
--- Phyton/My Photo Album.py
+++ Phyton/My Photo Album.py
@@ -1,5 +1,5 @@
 from tkinter import *   
-from tkinter import messaggebox
+from tkinter import messagebox
 from tkinter import messagebox
 from PIL import Image, ImageTk
 window = Tk()
